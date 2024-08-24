b97c32739a58a31a119e2b5c12d085240266672b
conghieu26 <hieunc00@gmail.com>
2024-08-24 18:32:04 +0700

    Update-user-template

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,18 +1,59 @@
 import * as React from "react";
-import { Link, createBrowserRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import { UserTemplate } from "../atomic/templates/user/user.template";
+
+const Home = lazy(() => import("../atomic/pages/home/index"));
+const Contact = lazy(() => import("../atomic/pages/contact/index"));
+const News = lazy(() => import("../atomic/news/index"));
+const Register = lazy(() => import("../atomic/pages/Register/index"));
+const Login = lazy(() => import("../atomic/pages/Login/index"));
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
+    element: <UserTemplate></UserTemplate>,
+    children: [
+      {
+        path: "home",
+        element: (
+          <Suspense fallback={<>Loading...</>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "contact",
+        element: (
+          <Suspense fallback={<>Loading...</>}>
+            <Contact />
+          </Suspense>
+        ),
+      },
+      {
+        path: "news",
+        element: (
+          <Suspense fallback={<>Loading...</>}>
+            <News />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+  {
+    path: "register",
     element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
+      <Suspense fallback={<>Loading...</>}>
+        <Register />
+      </Suspense>
     ),
   },
   {
-    path: "about",
-    element: <div>About</div>,
+    path: "login",
+    element: (
+      <Suspense fallback={<>Loading...</>}>
+        <Login />
+      </Suspense>
+    ),
   },
 ]);
